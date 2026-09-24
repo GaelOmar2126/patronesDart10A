5bc7c8b8631a6aaf17dfbde2ec9768fb227271b4
Gael Omar <PalaciosOmar2126@outlook.es>
2026-09-24 14:24:14 -0600

    Ejemplo sin singelton

---
+++ singeltone/main.dart
@@ -0,0 +1,11 @@
+import 'configuracion.dart';
+
+void main() {
+  final pantallaInicio = configuracion('es');
+  final pantallaPerfil = configuracion('es');
+
+  pantallaInicio.idioma = 'en';
+
+  print('Idioma de la pantalla de inicio: ${pantallaInicio.idioma}');
+  print('Idioma de la pantalla de perfil: ${pantallaPerfil.idioma}');
+}

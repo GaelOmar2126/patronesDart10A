26c9e83f623beac2e9769ad56b03a6efe464d8a4
Gael Omar <PalaciosOmar2126@outlook.es>
2026-09-24 14:51:20 -0600

    Ejemplo con singleton

--- singeltone/configuracion.dart
+++ singeltone/configuracion.dart
@@ -1,5 +1,12 @@
 class configuracion {
-  String? idioma;
+  //Es un constructor privado
+  configuracion._();
 
-  configuracion(this.idioma);
+  // lo que es static vivr durante todo el ciclo de vida de la aplicacion
+  // final: no se puede cambiar el valor de la variable
+  static final configuracion _instancia = configuracion._();
+
+  //Es un constyructor de fabrica que devuelve la misma instancia de la clase
+  factory configuracion() => _instancia;
+  String idioma = 'es';
 }
--- singeltone/main.dart
+++ singeltone/main.dart
@@ -1,11 +1,20 @@
 import 'configuracion.dart';
 
 void main() {
-  final pantallaInicio = configuracion('es');
-  final pantallaPerfil = configuracion('es');
+  final pantallaInicio = configuracion();
+
+  final pantallaPerfil = configuracion();
+
+  print('Idioma de la pantalla de inicio: ${pantallaInicio.idioma}');
+  print('Idioma de la pantalla de perfil: ${pantallaPerfil.idioma}');
 
   pantallaInicio.idioma = 'en';
 
   print('Idioma de la pantalla de inicio: ${pantallaInicio.idioma}');
   print('Idioma de la pantalla de perfil: ${pantallaPerfil.idioma}');
+
+  print( identical(pantallaInicio, pantallaPerfil)
+        ? 'Ambas pantallas comparten la misma instancia.'
+        : 'Las pantallas son instancias diferentes.',
+  );
 }
